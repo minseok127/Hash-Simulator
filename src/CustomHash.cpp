39c95cb684443f321e9497da19a7bd445325ac01
belowlow <yms127@naver.com>
2021-07-11 22:18:24 +0900

    keyset

--- src/CustomHash.cpp
+++ src/CustomHash.cpp
@@ -1,6 +1,6 @@
 #include "../include/types.h"
 
-void CustomHash_32(const void* key, int, uint32_t, void* out)
+void CustomHash_32(const void* key, int length, uint32_t, void* out)
 {
     *(uint32_t*)out = *(uint32_t*)key;
 }
